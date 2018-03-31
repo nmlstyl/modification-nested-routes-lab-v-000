17339fbd301a12db2997037cec4819ec17e2f5b7
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-03-31 23:37:02 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,7 +3,11 @@ module ArtistsHelper
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
 
-  def artist_select(path, name)
-
+  def artist_select(artist, song)
+    if song.artist == artist
+      artist.name
+    else
+      f.collection_select(:artist_id, Artist.all, :id, :name)
+    end
   end
 end
